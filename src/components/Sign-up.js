e77e4493398bb406bf84b7158cc7215d89740800
Saravanan-e-2003 <motts2003@gmail.com>
2024-12-21 00:50:07 +0530

    sign-page added(frontend)

--- src/components/Sign-up.js
+++ src/components/Sign-up.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { doSignInWithEmailAndPassword,doSignInWithGoogle } from "../firebase/auth";
 import { useAuth } from "../context/authContext";
 
-const Login = () => {
+const SignUp = () => {
     const {userLoggedIn} = useAuth;
     // const [email, setEmail] = useState("motts2003@gmail.com");
     const [email, setEmail] = useState('motts2003@gmail.com');
@@ -24,6 +24,11 @@ const handleSubmit = async (e) => {
      // Navigate to MainPage after submit
 };
 
+const LoginPage = () => {
+    navigate("/");
+};
+
+
 const onGoogleSignIn = async(e) =>{
     e.preventDefault();
     if(!isSigningIn){
@@ -34,14 +39,10 @@ const onGoogleSignIn = async(e) =>{
     }
 };
 
-const SignUpPage = () => {
-    navigate("/signup");
-};
-
     return (
         <div className="flex justify-center items-center h-screen bg-gray-100">
             <div className="bg-white shadow-lg rounded-lg p-8 w-96">
-                <h2 className="text-2xl font-bold text-center mb-6">Login</h2>
+                <h2 className="text-2xl font-bold text-center mb-6">SignUp</h2>
                 <form className="space-y-4" onSubmit={handleSubmit}>
                 <div>
                     <label className="block text-sm font-medium text-gray-700">Email</label>
@@ -74,10 +75,10 @@ const SignUpPage = () => {
         </form>
         <p className="text-gray-400 font-bold mx-32 m">---or---</p>
         <button
+            onClick={LoginPage}
             className="w-32 mx-24 py-1 px-1 hover:underline text-[#635fc7] font-bold rounded-3xl transition duration-300"
-            onClick={SignUpPage}
         >
-            SignUp
+            Login
         </button>
         
     </div>
@@ -85,4 +86,4 @@ const SignUpPage = () => {
     );
 };
 
-export default Login;
+export default SignUp;
